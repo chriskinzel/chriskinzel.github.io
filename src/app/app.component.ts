e216db71479301c156742335c1a68dbba1195b16
Chris Kinzel <chriskinzel13@gmail.com>
2020-11-21 11:34:50 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ck-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'personal-website';
-}
+export class AppComponent {}
